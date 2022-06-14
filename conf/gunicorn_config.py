dac206c38b2d9a03e60920ecc937f75040f2baaa
eliusmgani <eliasmgani@gmail.com>
2022-06-14 19:38:20 +0300

    remove gunicorn config

--- conf/gunicorn_config.py
+++
@@ -1,5 +0,0 @@
-# command = "/home/dev2/urlshortener_ellipsis/env/bin/gunicorn"
-# wsgi_app = "project.wsgi:application"
-# pythonpath = "/home/dev2/ellipsis_laravel_task/env/bin/python"
-# bind = "206.189.177.28:8000"
-# workers = 3 
